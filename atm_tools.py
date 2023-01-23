9220e44a7025d74e1ed436808759490ecbe06a02
Y.Nakamura <ny16274main@outlook.jp>
2023-01-23 13:51:21 +0900

    webmのリンクと曲名の取得コードを作成

--- atm_tools.py
+++ atm_tools.py
@@ -1,8 +1,15 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import sys
+from theme_props import Props as tp
 
-class Theme_props:
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
+args = sys.argv
+url = args[1]
+
+theme_props = tp(url)
+print(theme_props.theme_url)
+print(theme_props.anime_url)
+theme_props.download_webm()
+print(theme_props.webm_url)
+theme_props.rename()
--- theme_props.py
+++ theme_props.py
@@ -1,7 +1,29 @@
 import re
+import requests
+from bs4 import BeautifulSoup
+import json
+from lxml import html
 
-class Theme_props:
+class Props:
     def __init__(self, theme_url):
-        self.url = theme_url
-        # 最後のスラッシュ以降を取得
+        self.theme_url = theme_url
         self.anime_url = theme_url[:theme_url.rfind('/')]
+
+    def dump_web_page(self, url):
+        r = requests.get(url)
+        s = BeautifulSoup(r.text, 'html.parser')
+        return s
+
+    def download_webm(self):
+        self.theme_soup = self.dump_web_page(self.theme_url)
+        s = self.theme_soup
+        self.webm_url = s.find('meta', attrs={'name':'og:video','content':re.compile(r'v\.animethemes\.moe')}).get('content')
+        
+
+    def rename(self):
+        atm_theme_name = html.fromstring(str(self.theme_soup))
+        atm_theme_name = atm_theme_name.xpath('/html/body/div/div[1]/div[2]/div[1]/div/span/span[1]/text()')
+        print(atm_theme_name)
+        
+        
+
