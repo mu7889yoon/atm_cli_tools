001270d9f4b2e2c52043908da5c3a2ff412569ad
YNakamura7889 <ny16274main@outlook.jp>
2023-02-01 15:39:13 +0900

    webm,slug_anime_name,aDB_urlの取得コードを追加

--- theme.py
+++ theme.py
@@ -4,26 +4,22 @@ from bs4 import BeautifulSoup
 import json
 from lxml import html
 
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+
 class Props:
     def __init__(self, theme_url):
         self.theme_url = theme_url
-        self.anime_url = theme_url[:theme_url.rfind('/')]
+        anime_url = theme_url[:theme_url.rfind('/')]
+        self.slug_anime_name = anime_url[anime_url.rfind('/')+1:]
+        self.theme_soup  = BeautifulSoup(requests.get(self.theme_url, headers=headers).text, 'html.parser')
+        self.webm_url = self.theme_soup.find('meta', content=re.compile('https://v.animethemes.moe/')).get('content')
 
-    def dump_web_page(self, url):
-        r = requests.get(url)
-        s = BeautifulSoup(r.text, 'html.parser')
-        return s
-
-    def download_webm(self):
-        self.theme_soup = self.dump_web_page(self.theme_url)
-        s = self.theme_soup
-        self.webm_url = s.find('meta', attrs={'name':'og:video','content':re.compile(r'v\.animethemes\.moe')}).get('content')
-        
-
-    def rename(self):
-        atm_theme_name = html.fromstring(str(self.theme_soup))
-        atm_theme_name = atm_theme_name.xpath('/html/body/div/div[1]/div[2]/div[1]/div/span/span[1]/text()')
-        print(atm_theme_name)
-        
-        
-
+    def renamer(self):
+        self.en_theme_name = self.theme_soup.find('span', color='text-primary').text
+        atm_api = requests.get('https://api.animethemes.moe/anime/'+self.slug_anime_name+'?include=resources')
+        atm_api = atm_api.json()['anime']['resources']
+        for i in atm_api:
+            if i['site'] == 'aniDB':
+                self.aDB_url = i['link']
+                break
+        print(self.aDB_url)
