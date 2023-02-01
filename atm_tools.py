2caa7ad118684227b474885309a8c1bf02c3c3bf
YNakamura7889 <ny16274main@outlook.jp>
2023-02-01 18:46:54 +0900

    fix option

--- atm_tools.py
+++ atm_tools.py
@@ -4,9 +4,9 @@ from theme import Props
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072')
 # -f でオプションを指定する
-parser.add_argument('-f','--fast', help='Without rename, Just Download', action='store_true')
+parser.add_argument('-f', '--fast', help='Without rename, Just Download', action='store_true')
 args = parser.parse_args()
 url = args.url
-option = '-f' if args.f else None
+option = '-f' if args.fast else None
 
 props = Props(url, option)
