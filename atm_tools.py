71900fc79fde8661c83348087f98ff63d96c8fcc
YNakamura7889 <ny16274main@outlook.jp>
2023-02-01 18:27:09 +0900

    add --fast option

--- atm_tools.py
+++ atm_tools.py
@@ -4,7 +4,7 @@ from theme import Props
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072')
 # -f でオプションを指定する
-parser.add_argument('-f', help='Without rename, Just Download', action='store_true')
+parser.add_argument('-f','--fast', help='Without rename, Just Download', action='store_true')
 args = parser.parse_args()
 url = args.url
 option = '-f' if args.f else None
