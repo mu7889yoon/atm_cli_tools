9e931a40273ed291dd436f731b37f219fe788bf8
Y.Nakamura <gp20a110@oecu.jp>
2023-02-08 09:37:28 +0900

    add -o --outpu option

--- atm_tools.py
+++ atm_tools.py
@@ -1,12 +1,14 @@
 import argparse
 from theme import Props
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072')
-# -f でオプションを指定する
 parser.add_argument('-f', '--fast', help='Without rename, Just Download', action='store_true')
+parser.add_argument('-o', '--output', help='Output file name')
 args = parser.parse_args()
 url = args.url
 option = '-f' if args.fast else None
-
+if args.output:
+    os.chdir(args.output)
 props = Props(url, option)
