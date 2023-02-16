bb12051ed57deed3cbad05dcb4d59e30b4317cef
Y.Nakamura <gp20a110@oecu.jp>
2023-02-16 14:05:19 +0900

    add -d --dir option -d DIR --dir DIR でファイルの保存先を変更する

--- atm_tools.py
+++ atm_tools.py
@@ -1,12 +1,14 @@
 import argparse
 from theme import Props
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072')
-# -f でオプションを指定する
 parser.add_argument('-f', '--fast', help='Without rename, Just Download', action='store_true')
+parser.add_argument('-d', '--dir', help='Directory to save the theme', default=os.getcwd())
 args = parser.parse_args()
 url = args.url
 option = '-f' if args.fast else None
-
+directory = args.dir
+os.chdir(directory)
 props = Props(url, option)
