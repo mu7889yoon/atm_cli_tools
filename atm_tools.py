e156b8350ef4514de19516efdb3ef0329d656e08
Y.Nakamura <gp20a110@oecu.jp>
2023-02-21 14:20:45 +0900

    add interactive mode

--- atm_tools.py
+++ atm_tools.py
@@ -3,7 +3,7 @@ from theme import Props
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072')
+parser.add_argument('url', help='URL of the theme page EX: https://animethemes.moe/anime/hayate_no_gotoku/OP2-BD1072', nargs='?')
 parser.add_argument('-f', '--fast', help='Without rename, Just Download', action='store_true')
 parser.add_argument('-d', '--dir', help='Directory to save the theme', default=os.getcwd())
 args = parser.parse_args()
@@ -11,5 +11,13 @@ url = args.url
 option = '-f' if args.fast else None
 directory = args.dir
 os.chdir(directory)
-props = Props(url, option)
-
+if url != None:
+    props = Props(url, option)
+elif url == None:
+    print('Input Animethemes URL')
+    while True:
+        url = input('ATDL >> ')
+        if url == 'exit':
+            break
+        props = Props(url, option)
+        print('')
