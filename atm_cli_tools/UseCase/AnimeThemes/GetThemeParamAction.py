8c34aa1af039cbdc38e237372a1d81178bca2e26
Y.Nakamura <gp20a110@oecu.jp>
2024-01-24 10:15:38 +0900

    引数を変更

--- atm_cli_tools/UseCase/AnimeThemes/GetThemeParamAction.py
+++ atm_cli_tools/UseCase/AnimeThemes/GetThemeParamAction.py
@@ -1,8 +1,10 @@
 from atm_cli_tools.helper import *
 from atm_cli_tools.UseCase.AnimeThemes.GetAllThemesParamAction import GetAllThemesParamAction
+from atm_cli_tools.UseCase.GetSlugAction import GetSlugAction
 
-def GetThemeParamAction(slug, type):
+def GetThemeParamAction(url):
+    slug = GetSlugAction(url)
     themes = GetAllThemesParamAction(slug)
     for theme in themes:
-        if theme['type'] == type:
+        if theme['theme_url'] == url:
             return theme
