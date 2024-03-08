9191c7d1a5478c8b04b3e127cb5ad2ef4536a971
Y.Nakamura <gp20a110@oecu.jp>
2024-03-08 12:14:38 +0900

    downloadからpathを受け取るように

--- atm_cli_tools/Server/Controller/ThemeControllerServer.py
+++ atm_cli_tools/Server/Controller/ThemeControllerServer.py
@@ -2,34 +2,20 @@ from atm_cli_tools.Controller.ThemeController import ThemeControllerClass
 from atm_cli_tools.Server.UseCase.Theme.DownloadAction import DownloadAction
 from atm_cli_tools.Server.UseCase.Spreadsheet.StoreWebmUrlAction import StoreWebmUrlAction
 from atm_cli_tools.Server.UseCase.Drive.GetIdAction import GetIdAction
-from atm_cli_tools.UseCase.Theme.GetAction import GetAction
 from atm_cli_tools.Server.UseCase.Drive.MakeLinkAction import MakeLinkAction
-from atm_cli_tools.UseCase.Theme.DetermineFilenameAction import DetermineFilenameAction
 
 from atm_cli_tools.Model.Anime import Anime
 
 class ThemeControllerForServerClass(ThemeControllerClass):
-    def __init__(self, theme: dict, Anime: Anime, path: str, daily_folder_id: str):
-        self.theme = GetAction(theme, Anime)
-        self.filename = DetermineFilenameAction(
-            self.theme.name,
-            self.theme.anime_title,
-            self.theme.type,
-            self.theme.artist,
-            '.mp4',
-        )
+    def __init__(self, theme: dict, Anime: Anime, path: str, daily_folder_id: str)->None:
+        super().__init__(theme, Anime, '.mp4')
         
         self.path = path
         self.daily_folder_id = daily_folder_id
-        DownloadAction(self.path, self.theme.webm_url, self.filename)
-        file_id = GetIdAction(self.path + '/' + self.filename, 15)
-        StoreWebmUrlAction(self.theme.webm_url)
-        MakeLinkAction(self.daily_folder_id, file_id)
         
     def download(self) -> None:
-        DownloadAction(self.path, self.Theme.webm_url, self.filename)
-        file_id = GetIdAction(self.path + '/' + self.Theme.filename, 15)
+        path = DownloadAction(self.path, self.theme.webm_url, self.filename)
+        file_id = GetIdAction(path, 5.0)
         StoreWebmUrlAction(self.Theme.webm_url)
         MakeLinkAction(self.daily_folder_id, file_id)
-        # ディスコードに通知
     
--- atm_cli_tools/Server/UseCase/Theme/DownloadAction.py
+++ atm_cli_tools/Server/UseCase/Theme/DownloadAction.py
@@ -1,9 +1,10 @@
 import subprocess
 
-def DownloadAction(path: str, webm_url: str, filename: str)-> None:
+def DownloadAction(path: str, webm_url: str, filename: str)-> str:
     subprocess.run([
         'ffmpeg', '-i',
         webm_url, '-c:v',
         'libx264', '-crf',
         '20', path +'/'+ filename,
     ])
+    return path +'/'+ filename
