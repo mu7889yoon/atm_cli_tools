fa3aa40cb9c367eabed057a06d1ebad5700e39b0
Y.Nakamura <gp20a110@oecu.jp>
2024-03-08 12:38:11 +0900

    機能の削除

--- atm_cli_tools/UseCase/AnimeThemes/GetEnTitleAction.py
+++ atm_cli_tools/UseCase/AnimeThemes/GetEnTitleAction.py
@@ -1,5 +1,5 @@
-from atm_cli_tools.helper import *
+# from atm_cli_tools.helper import *
 
-def GetEnTitleAction(slug):
-    json_data = GetJson('https://api.animethemes.moe/anime/'+slug)
-    return json_data['anime']['name']
+# def GetEnTitleAction(slug):
+#     json_data = GetJson('https://api.animethemes.moe/anime/'+slug)
+#     return json_data['anime']['name']
